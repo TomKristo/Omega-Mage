2f2e8f980e406d9a26dbd542b29762c6905d82da
Tomislav Kristo <31782349+TomKristo@users.noreply.github.com>
2017-12-10 10:57:03 -0500

    Added Movement debugger to Mage script and mage spawn in Layout Tiles script

--- Assets/__Scripts/Mage.cs
+++ Assets/__Scripts/Mage.cs
@@ -3,12 +3,168 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+public enum MPhase
+{
+    idle,
+    down,
+    drag
+}
+
+[System.Serializable]
+public class MouseInfo
+{
+    public Vector3 loc;
+    public Vector3 screenLoc;
+    public Ray ray;
+    public float time;
+    public RaycastHit hitInfo;
+    public bool hit;
+
+
+    public RaycastHit Raycast()
+    {
+        hit = Physics.Raycast(ray, out hitInfo);
+        return (hitInfo);
+    }
+
+    public RaycastHit Raycast(int mask)
+    {
+        hit = Physics.Raycast(ray, out hitInfo, mask);
+        return (hitInfo);
+    }
+}
+
 public class Mage : PT_MonoBehaviour {
 
     static public Mage S;
+    static public bool DEBUG = true;
+
+    public float mTapTime = 0.1f;
+    public float mDragDist = 5;
+    public float activeScreenWidth = 1;
+
+    public bool ________________;
+
+    public MPhase mPhase = MPhase.idle;
+    public List<MouseInfo> mouseInfos = new List<MouseInfo>();
 
     void Awake()
     {
         S = this;
+        mPhase = MPhase.idle;
+    }
+
+    void Update()
+    {
+        bool b0Down = Input.GetMouseButtonDown(0);
+        bool b0Up = Input.GetMouseButtonUp(0);
+
+        bool inActiveArea = (float)Input.mousePosition.x / Screen.width < activeScreenWidth;
+
+        if (mPhase == MPhase.idle)
+        {
+            if (b0Down && inActiveArea)
+            {
+                mouseInfos.Clear();
+                AddMouseInfo();
+
+                if (mouseInfos[0].hit)
+                {
+                    MouseDown();
+                    mPhase = MPhase.down;
+                }
+            }
+        }
+
+        if (mPhase == MPhase.down)
+        {
+            AddMouseInfo();
+
+            if (b0Up)
+            {
+                MouseTap();
+                mPhase = MPhase.idle;
+            }
+            else if (Time.time - mouseInfos[0].time > mTapTime)
+            {
+                float dragDist = (lastMouseInfo.screenLoc - mouseInfos[0].screenLoc).magnitude;
+
+                if (dragDist >= mDragDist)
+                {
+                    mPhase = MPhase.drag;
+                }
+            }
+        }
+
+        if (mPhase == MPhase.drag)
+        {
+            AddMouseInfo();
+
+            if (b0Up)
+            {
+                MouseDragUp();
+                mPhase = MPhase.idle;
+            }
+            else
+            {
+                MouseDrag();
+            }
+        }
+    }
+
+    MouseInfo AddMouseInfo()
+    {
+        MouseInfo mInfo = new MouseInfo();
+        mInfo.screenLoc = Input.mousePosition;
+        mInfo.loc = Utils.mouseLoc;
+        mInfo.ray = Utils.mouseRay;
+
+        mInfo.time = Time.time;
+        mInfo.Raycast();
+
+        if (mouseInfos.Count == 0)
+        {
+            mouseInfos.Add(mInfo);
+        }
+        else
+        {
+            float lastTime = mouseInfos[mouseInfos.Count - 1].time;
+
+            if (mInfo.time != lastTime)
+            {
+                mouseInfos.Add(mInfo);
+            }
+        }
+
+        return (mInfo);
+    }
+
+    public MouseInfo lastMouseInfo
+    {
+        get
+        {
+            if (mouseInfos.Count == 0) return (null);
+            return (mouseInfos[mouseInfos.Count - 1]);
+        }
+    }
+
+    void MouseDown()
+    {
+        if (DEBUG) print("Mage.MouseDown()");
+    }
+
+    void MouseTap()
+    {
+        if (DEBUG) print("Mage.MouseTap()");
+    }
+
+    void MouseDrag()
+    {
+        if (DEBUG) print("Mage.MouseDrag()");
+    }
+
+    void MouseDragUp()
+    {
+        if (DEBUG) print("Mage.MouseDragUp()");
     }
 }
